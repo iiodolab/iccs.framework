2493f6e26166565e2eed28ccecdcaf459b070a0d
iodo <jnwochoi@gmail.com>
2021-01-21 12:45:59 +0900

    .

---
+++ Iccs.Framework.Solutions/Iodo.Iccs.Framework/Services/CoreService.cs
@@ -0,0 +1,52 @@
+﻿using StackExchange.Redis;
+using System.Threading;
+using System.Threading.Tasks;
+
+/******************************************************************************
+ * Abstract Class CoreService
+ * 
+ *                                                   author: Jinwoo Choi, PhD.
+ *                                                 organization: Kookmin Univ.
+ *                                                           date: 2020.10.28
+ *                                                           
+ *****************************************************************************/
+namespace Iodo.Iccs.Framework.Services
+{
+    public abstract class CoreService : TaskService
+    {
+        #region - Ctors -
+        public CoreService(MessageService messageService)
+        {
+            MessageService = messageService;
+        }
+        #endregion
+
+        protected abstract void ProcessChannel1Message(object sender, ChannelMessage message);
+        protected abstract void ProcessChannel2Message(object sender, ChannelMessage message);
+
+        #region - Implements overrides -
+        protected override Task RunTask(CancellationToken token = default)
+        {
+            return Task.Run(delegate { RegisterEventHandelers(); });
+        }
+
+        public override void Stop()
+        {
+            MessageService.Channel1EventHandler -= ProcessChannel1Message;
+            MessageService.Channel2EventHandler -= ProcessChannel2Message;
+        }
+        #endregion
+
+        #region - Procedures -
+        private void RegisterEventHandelers()
+        {
+            MessageService.Channel1EventHandler += ProcessChannel1Message;
+            MessageService.Channel2EventHandler += ProcessChannel2Message;
+        }
+        #endregion
+
+        #region - Overrides -
+        private MessageService MessageService { get; }
+        #endregion
+    }
+}
---
+++ Iccs.Framework.Solutions/Iodo.Iccs.Framework/Services/MessageService.cs
@@ -0,0 +1,56 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+/******************************************************************************
+ * Abstract Class MessageService
+ * 
+ *                                                   author: Jinwoo Choi, PhD.
+ *                                                 organization: Kookmin Univ.
+ *                                                           date: 2020.10.28
+ *                                                           
+ *****************************************************************************/
+
+namespace Iodo.Iccs.Framework.Services
+{
+    public abstract class MessageService 
+        : TaskService
+    {
+        #region - Ctors -
+        public MessageService(ISubscriber subscriber, string nameChannel)
+        {
+            Subscriber = subscriber;
+            NameChannel = nameChannel;
+        }
+        #endregion
+
+        #region - Implements asbtracts -
+        protected override async Task RunTask(CancellationToken token = default)
+        {
+            await Task.Run(delegate { RegisterSubscribers(); }, token);
+        }
+
+        public override void Stop()
+        {
+        }
+        #endregion
+        #region - Procedures -
+        protected virtual void RegisterSubscribers()
+        {  
+           Subscriber.Subscribe(NameChannel).OnMessage((message) =>
+                    Channel1EventHandler?.Invoke(this, message));
+        }
+        #endregion
+
+        #region - Properties -
+        protected ISubscriber Subscriber { get; }
+        private string NameChannel { get; }
+        #endregion
+
+        #region - Delegators for EventHandlers -
+        public event EventHandler<ChannelMessage> Channel1EventHandler;
+        public event EventHandler<ChannelMessage> Channel2EventHandler;
+        #endregion
+    }
+}
