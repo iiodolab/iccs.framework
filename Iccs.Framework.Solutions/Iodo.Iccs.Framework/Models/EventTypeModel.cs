3933dba2ac56691325c1670ec5311a7c5b9827d3
iodo <jnwochoi@gmail.com>
2021-01-23 02:31:09 +0900

    .

--- Iccs.Framework.Solutions/Iodo.Iccs.Framework/Models/EventTypeModel.cs
+++ Iccs.Framework.Solutions/Iodo.Iccs.Framework/Models/EventTypeModel.cs
@@ -3,8 +3,20 @@
  * 
  *                                                   author: Jinwoo Choi, PhD.
  *                                                 organization: Kookmin Univ.
- *                                                           date: 2021.01.15
- *                                                           
+ *                                                           date: 2020.10.28
+ *
+ * Description
+ *  침입 (90)
+ *      - Intrusion = 0x5A
+ *  연결보고
+ *      - Connection = 0x68
+ *  조치보고 (192)        
+ *      - Action = 0xC0
+ *  장애보고 (115)
+ *      - Fault = 0x73
+ *  풍량모드 (118)
+ *      - WindyMode = 0x76
+ *  
  *****************************************************************************/
 
 namespace Iodo.Iccs.Framework.Models
