5d6587a7ac6aba4770cf67543ff077810f7c2716
iodo <jnwochoi@gmail.com>
2021-01-25 04:33:11 +0900

    .

---
+++ Iccs.Framework.Solutions/Iodo.Iccs.Framework/ViewModels/DialogShellViewModel.cs
@@ -0,0 +1,27 @@
+﻿using Caliburn.Micro;
+using System.Linq;
+using System.Collections.Generic;
+
+/******************************************************************************
+* Abstract Class DialogShellViewModel
+* 
+*                                                   author: Jinwoo Choi, PhD.
+*                                                 organization: Kookmin Univ.
+*                                                           date: 2020.10.28
+*                                                           
+*****************************************************************************/
+
+namespace Iodo.Iccs.Framework.ViewModels
+{
+    public abstract class DialogShellViewModel
+        : Conductor<IScreen>.Collection.OneActive
+    {
+        public DialogShellViewModel(IEnumerable<IScreen> enumerator)
+        {
+            foreach (var item in enumerator)
+            {
+                Items.Add(item);
+            }
+        }
+    }
+}
