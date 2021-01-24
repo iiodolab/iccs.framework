e3d0cb4c14ef4809190bd396bcdd9f1c2c42a295
iodo <jnwochoi@gmail.com>
2021-01-25 03:06:53 +0900

    .

---
+++ Iccs.Framework.Solutions/Iodo.Iccs.Framework/ViewModels/EventShellViewModel.cs
@@ -0,0 +1,33 @@
+﻿using Caliburn.Micro;
+
+/******************************************************************************
+ * Class EventShellViewModel
+ * 
+ *                                                   author: Jinwoo Choi, PhD.
+ *                                                 organization: Kookmin Univ.
+ *                                                           date: 2020.10.28
+ *                                                           
+ *****************************************************************************/
+
+namespace Iodo.Iccs.Framework.ViewModels
+{
+    public class EventShellViewModel<T1, T2>
+        : Conductor<IScreen>.Collection.OneActive
+        where T1 : IScreen
+        where T2 : IScreen
+    {
+        #region - Ctors -
+        public EventShellViewModel(
+              T1 preEventListViewModel
+            , T2 postEventListViewModel)
+        {
+            Items.Add(preEventListViewModel);
+            Items.Add(postEventListViewModel);
+            ActiveItem = Items[0];
+        }
+        #endregion
+
+        #region - Attributes -
+        #endregion
+    }
+}
