56367b4b965db6a98f8efc287270e5f767d99495
Jinwoo Choi <jnwochoi@gmail.com>
2021-01-25 17:50:54 +0900

    .

---
+++ Iccs.Framework.Solutions/Iodo.Iccs.Framework/ViewModels/CanvasEntityViewModel.cs
@@ -0,0 +1,41 @@
+﻿using Caliburn.Micro;
+
+/******************************************************************************
+ * Abstract Class CanvasEntityViewModel
+ * 
+ *                                                   author: Jinwoo Choi, PhD.
+ *                                                 organization: Kookmin Univ.
+ *                                                           date: 2020.10.28
+ *                                                           
+ *****************************************************************************/
+
+namespace Iodo.Iccs.Framework.ViewModels
+{
+    public abstract class CanvasEntityViewModel<T>
+        : Screen
+    {
+        #region - Ctors
+        public CanvasEntityViewModel(T symbolProvider, bool visibility = true)
+        {
+            SymbolProvider = symbolProvider;
+            Visibility = visibility;
+        }
+        #endregion
+
+        #region - Properties -
+        public T SymbolProvider { get; }
+        public bool Visibility {
+            get => visibility;
+            set
+            {
+                visibility = value;
+                NotifyOfPropertyChange(() => Visibility);
+            }
+        }
+        #endregion
+
+        #region - Attributes -
+        private bool visibility;
+        #endregion
+    }
+}
--- Iccs.Framework.Solutions/Iodo.Iccs.Framework/ViewModels/DialogShellViewModel.cs
+++ Iccs.Framework.Solutions/Iodo.Iccs.Framework/ViewModels/DialogShellViewModel.cs
@@ -2,13 +2,13 @@
 using System.Collections.Generic;
 
 /******************************************************************************
-* Abstract Class DialogShellViewModel
-* 
-*                                                   author: Jinwoo Choi, PhD.
-*                                                 organization: Kookmin Univ.
-*                                                           date: 2020.10.28
-*                                                           
-*****************************************************************************/
+ * Abstract Class DialogShellViewModel
+ * 
+ *                                                   author: Jinwoo Choi, PhD.
+ *                                                 organization: Kookmin Univ.
+ *                                                           date: 2020.10.28
+ *                                                           
+ *****************************************************************************/
 
 namespace Iodo.Iccs.Framework.ViewModels
 {
